49839ad874ae76790db75be1c9b9c34d8745a588
Miguel Alonso <60105368+MiguelAlonso1@users.noreply.github.com>
2021-09-01 18:53:10 -0400

    Add project files.

---
+++ PEM_SENIOR_PROJECT_BACK_END_TEST/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PEM_SENIOR_PROJECT_BACK_END_TEST.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
